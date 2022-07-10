228e0eae2e03667432c7c8fa79d08f984fb5176f
kyungmin Bang <shrmf12@naver.com>
2022-07-10 16:54:07 +0900

    2022-07-10주석 추가

--- SPI_protocol_module/SPI_master.v
+++ SPI_protocol_module/SPI_master.v
@@ -1,3 +1,9 @@
+//작성자 : 방경민
+//모듈 역할 : SPI 통신 모듈 
+//설계 목표 : CPOL,CPHA를 이용하여 4가지 모드를 사용하여 슬레이브에 데이터 전송방식을 결정
+//            테스트는 CPOL=0, CPHA=0의 값으로 기본 클럭 상태 0 & 상승엣지마다 데이터를 읽을 수 있도록 설계
+//             input된 데이터 값을 클럭마다 1비트씩 슬레이브에 MOSI데이터로 전송하고 전달된 MISO데이터를 읽는 역할
+
 `timescale 1ns/1ps
 module SPI_master(
     input clk,
@@ -14,32 +20,32 @@ module SPI_master(
     output reg slave_start);
 
 reg sampled = 1;
-reg data_flag = 1;
-reg CPOL;
-reg CPHA;
-reg [7:0] data_buffer;
+reg data_flag = 1; //0으로 변경시 데이터 READ WRITE 신호
+reg CPOL; //클럭의 기본 극성을 결정 REG
+reg CPHA; //어느 엣지에서 데이터를 전송할지 결정할 REG
+reg [7:0] data_buffer; // 데이터 저장 REG
 
 integer i = 0;
 integer count_1 = 0;
 integer count_2 = 0;
 
 always@(posedge clk) begin
-    if(load) data_buffer = i_data;
+    if(load) data_buffer = i_data; //load 신호를 받아서 버퍼 레지에 입력 데이터를 입력
     if(count_1 == 17 && data_flag == 1) begin
         slave_start = 0;
         count_1 = 0;
         count_2 = 0;
-    end if(i==8) begin
+    end if(i==8) begin //데이터 읽고 스기 완료시 초기화
         sampled = 0;
         slave_start = 0;
-    end if(start && data_flag) begin
+    end if(start && data_flag) begin //start, flag신호가 있을시 기본 설정 set
         data_flag = 0;
         count_1 = 0;
         count_2 = 0;
         CPOL = cpol;
         CPHA = cpha;
         i = 0;
-    end if(!data_flag) begin
+    end if(!data_flag) begin //MAIN) flag 신호 0 (read&write on) 상태에서 SCLK을 생성 CPHA=0 데이터버퍼 0 배열값을 MOSI에 입력
         if(count_1 ==0) begin
             SCLK = CPOL;
             count_1 = count_1 + 1;
@@ -48,7 +54,7 @@ always@(posedge clk) begin
                 MOSI = data_buffer[0];
                 count_2 = count_2 + 1;
             end
-        end else begin
+        end else begin //CPOL값을 확인하여 SCLK 기본상태와 slave 시작점을 결정
 
             SCLK = ~SCLK;
 
@@ -69,9 +75,9 @@ always@(posedge clk) begin
                 end
             end
         end
-        // data read, write
+        // data read, write 구문
         if(sampled == 1) begin
-            if(CPHA) begin // CPHA =1 write positive clk, read negative clk 
+            if(CPHA) begin // CPHA =1 write 상승 clk, read 하강 clk 
                 if(count_2 == 2) begin
                     i = i + 1;
                     count_2 = 0;
@@ -87,7 +93,7 @@ always@(posedge clk) begin
                     count_2 = count_2 + 1;    
                 end
             end 
-            if(!CPHA) begin //CPHA =0 write negative clk, read positive clk
+            if(!CPHA) begin //CPHA =0 write 하강 clk, read 상승 clk
                 if(count_2 == 2) begin
                     i = i + 1;
                     count_2 = 0;
--- SPI_protocol_module/SPI_master_tb.v
+++ SPI_protocol_module/SPI_master_tb.v
@@ -1,3 +1,8 @@
+//작성자 : 방경민
+//모듈 역할 : SPI 통신 모듈 
+//설계 목표 : SPI MASTER 모듈 TB용 input data를 slave에 보낸 가상 MISO 데이터 환경을 만들어 slave에서 보내온 MISO와 
+//input data의 비교 역할을 수행하고 웨이브 폼상에서 확인
+
 `timescale 1ns/1ps
 module SPI_master_tb;
     reg load;
@@ -22,9 +27,9 @@ reg [7:0] r_MISO;
 integer j;
 
 always
-    #2 clk = ~clk;
+    #2 clk = ~clk; //기본 클럭 생성
 initial begin
-    test();
+    test(); //추후 CPOL,CPHA를 통해 4가지 모드를 테스트하기 위해 task 구문으로 생성및테스트
 end
 
 task test();
@@ -32,16 +37,16 @@ begin
     clk = 0;
     cpol = 0;
     cpha = 0;
-    i_data = 'ha5;
+    i_data = 'ha5; //master에 전송할 data 
     start = 1;
-    r_MISO = 'hba;
+    r_MISO = 'hba; //slave에서 전송될 data
     @(posedge clk)
     load = 1;
 $strobe("%d  \t%b \t%b \t%b \t%b \t%b \t%d  \t%b     \t%b      \t%b  \t%b \t%b +ve clk, idel state",$time,clk,SCLK,slave,cpol,cpha,load,MOSI,i_data,data_read,MISO,slave_start);
     @(negedge clk)
     load = 0;
     start = 0;
-    for(j=0; j <= 7; j = j + 1) begin
+    for(j=0; j <= 7; j = j + 1) begin //상승엣지 마다 index를 활용하여 slave에서 전송해오는 MISO데이터를 가상으로 생성( 검증하기 위함)
         @(posedge clk)
         MISO = r_MISO[j];
         if(j==0)
@@ -52,12 +57,12 @@ $strobe("%d  \t%b \t%b \t%b \t%b \t%b \t%d  \t%b     \t%b      \t%b  \t%b \t%b +
         @(posedge clk)
 $strobe("%d  \t%b \t%b \t%b \t%b \t%b \t%d  \t%b     \t%b      \t%b  \t%b \t%b +ve clk, idel state",$time,clk,SCLK,slave,cpol,cpha,load,MOSI,i_data,data_read,MISO,slave_start);
         @(negedge clk)
-        if(j!=7)
+        if(j!=7)  
             r_MOSI[j+1]=MOSI;
             $display("found = %h",r_MOSI);
     end
 
-    for(i=0; i <= 7; i=i+1) begin
+    for(i=0; i <= 7; i=i+1) begin //데이터 비교 검증을 위한 테스트 구문
         if(data_read[i] != r_MISO[i]) begin
             $display(" [%d]data error, r_MISO = %b, data_read = %b",i,r_MISO[i],data_read[i]);
         end else begin
